f648912c7f90748d6c8478f993d6aca4dfe08485
burrelle <burrelle@oregonstate.edu>
2018-03-29 20:58:29 -0700

    PHP design patterns

---
+++ Adapter/index.php
@@ -0,0 +1,20 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Acme\Book;
+use Acme\BookInterface;
+use Acme\Kindle;
+use Acme\eReaderAdapter;
+use Acme\eBookInterface;
+
+class Person
+{
+	public function read(BookInterface $book)
+	{
+		$book->open();
+		$book->turnPage();
+	}
+}
+
+(new Person)->read(new eReaderAdapter(new Kindle));
---
+++ Decorator.php
@@ -0,0 +1,70 @@
+<?php
+
+// Decorator Design Pattern 
+
+interface CarService 
+{
+	public function getCost();
+	public function getDescription();
+}
+
+class BasicInspection implements CarService
+{
+
+    public function getCost()
+    {
+    	return 25;
+    }
+
+    public function getDescription()
+    {
+    	return 'Basic Inspection';
+    }
+}
+
+class OilChange implements CarService
+{
+	protected $carService;
+
+	public function __construct(CarService $carService)
+	{
+		$this->carService = $carService;
+	}
+
+
+	public function getCost()
+	{
+		return 29 + $this->carService->getCost();
+	}
+
+	public function getDescription()
+	{
+		return $this->carService->getDescription() . ', and an oil change';
+	}
+}
+
+class TireChange implements CarService
+{
+	protected $carService;
+
+	public function __construct(CarService $carService)
+	{
+		$this->carService = $carService;
+	}
+
+
+	public function getCost()
+	{
+		return 29 + $this->carService->getCost();
+	}
+
+	public function getDescription()
+	{
+		return $this->carService->getDescription() . ', and a tire change';
+	}
+}
+
+// echo (new TireChange(new OilChange(new BasicInspection())))->getCost();
+$service = (new OilChange(new BasicInspection()));
+echo $service->getDescription();
+
