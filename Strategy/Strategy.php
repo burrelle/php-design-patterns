a2c6e6044b655d345b7d986037a7f922e2460786
Evan Burrell <burrelle@oregonstate.edu>
2018-03-30 16:02:59 -0700

    Strategy design pattern

---
+++ Strategy/Strategy.php
@@ -0,0 +1,43 @@
+<?php
+
+// Encapsulate and make each one interchangable
+interface Logger
+{
+    public function log($data);
+}
+
+// Define a family of algorithms
+class LogToFile implements Logger
+{
+    public function log($data)
+    {
+        var_dump('log the data to the file');
+    }
+}
+
+class LogToDatabase implements Logger
+{
+    public function log($data)
+    {
+        var_dump('log the data to the database');
+    }
+}
+
+class LogToXWebService implements Logger
+{
+    public function log($data)
+    {
+        var_dump('log the data to the Saas');
+    }
+}
+
+class App 
+{
+    public function log($data, Logger $logger)
+    {
+        $logger->log($data);
+    }
+}
+
+$app = new App;
+$app->log('Data to be logged',new LogToDatabase);
