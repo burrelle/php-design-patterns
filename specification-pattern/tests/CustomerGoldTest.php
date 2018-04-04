73de5bd54dc62764740835786d1d3bb61ad8b329
burrelle <burrelle@oregonstate.edu>
2018-04-03 20:52:17 -0700

    Specification with a database

--- specification-pattern/tests/CustomerGoldTest.php
+++ specification-pattern/tests/CustomerGoldTest.php
@@ -8,8 +8,8 @@ class CustomerIsGoldTest extends \PHPUnit\Framework\TestCase
     function test_a_customer_is_gold_if_they_have_the_respective_type()
     {
         $spec = new CustomerIsGold;
-        $goldCustomer = new Customer('gold');
-        $silverCustomer = new Customer('silver');
+        $goldCustomer = new Customer(['type' => 'gold']);
+        $silverCustomer = new Customer(['type' => 'silver']);
         $this->assertTrue($spec->isSatisfiedBy($goldCustomer));
         $this->assertFalse($spec->isSatisfiedBy($silverCustomer));
     }
--- specification-pattern/tests/CustomersRepositoryTest.php
+++ specification-pattern/tests/CustomersRepositoryTest.php
@@ -1,6 +1,7 @@
 <?php
 
 require 'bootstrap.php';
+use Illuminate\Database\Capsule\Manager as DB;
 
 class CustomerRepositoryTest extends \PHPUnit\Framework\TestCase 
 {
@@ -9,25 +10,44 @@ class CustomerRepositoryTest extends \PHPUnit\Framework\TestCase
 
     function setUp()
     {
-        $this->customers = $customers = new CustomersRepository(
-            [
-                new Customer('gold'),
-                new Customer('bronze'),
-                new Customer('silver'),
-                new Customer('gold')
-            ]
-        );
+        $this->setUpDatabase();
+        $this->migrateTables();
+        $this->customers = new CustomersRepository;
+    }
+
+    protected function setUpDatabase()
+    {
+        $database = new DB;
+        $database->addConnection([
+            'driver' => 'sqlite',
+            'database' => ':memory:'
+        ]);
+        $database->bootEloquent();
+        $database->setAsGlobal();
+    }
+
+    protected function migrateTables()
+    {
+        DB::schema()->create('customers', function ($table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('type');
+            $table->timestamps();
+        });
+
+        Customer::create(['name' => 'Joe', 'type' => 'gold']);
+        Customer::create(['name' => 'Jane', 'type' => 'silver']);
     }
     
     function test_it_fetches_all_customers()
     {
         $results = $this->customers->all();
-        $this->assertCount(4, $results);
+        $this->assertCount(2, $results);
     }
 
     function test_it_fetches_all_customers_who_match_a_given_specification()
     {
         $results = $this->customers->bySpecification(new CustomerIsGold);
-        $this->assertCount(2, $results);
+        $this->assertCount(1, $results);
     }
 }
